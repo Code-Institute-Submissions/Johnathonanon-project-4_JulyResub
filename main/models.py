d2cf982ecde791b5d0c42b24c71bc43561dd0151
Johnathonanon <jconry21@gmail.com>
2022-03-23 21:01:17 +0000

    Add Advert Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Advert(models.Model):
+    title = models.CharField(max_length=100, unique=False)
+    slug = models.SlugField(max_length=100, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    firearm_make = models.CharField(max_length=100, unique=False)
+    firearm_model = models.CharField(max_length=100, unique=False)
+    calibre = models.CharField(max_length=100, unique=False)
+    price = models.IntegerField()
+    description = models.TextField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
