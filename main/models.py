b25ce70b7d7e6c625c7f0f8bd84888e803df6597
Johnathonanon <jconry21@gmail.com>
2022-03-24 13:23:20 +0000

    Create Message model, update Advert model, and migrate

--- main/models.py
+++ main/models.py
@@ -12,11 +12,30 @@ class Advert(models.Model):
     calibre = models.CharField(max_length=100, unique=False)
     price = models.IntegerField()
     description = models.TextField()
-    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='seller_ad')
     created_on = models.DateField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+
+
+class Message(models.Model):
+    advert = models.ForeignKey(
+        Advert, on_delete=models.CASCADE, related_name='message')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Message {self.body} by {self.name}"
+
