81ce6bae16055e739ec5b178667702250375fd7d
Johnathonanon <jconry21@gmail.com>
2022-03-24 19:43:14 +0000

    Create AdvertList view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Advert
 
-# Create your views here.
+
+class AdvertList(generic.ListView):
+    model = Advert
+    queryset = Advert.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 8
