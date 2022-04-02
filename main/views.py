90d3c8c19aed0501eba1e65a42fe8af1bcef4981
Johnathonanon <jconry21@gmail.com>
2022-04-02 21:06:37 +0000

    Create AdvertInfo view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Advert
 
 
@@ -8,3 +8,14 @@ class AdvertList(generic.ListView):
     queryset = Advert.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 8
+
+
+class AdvertInfo(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Advert.objects.filter(status=1)
+        advert = get_object_or_404(queryset, slug=slug)
+        
+        return render(request, "advert_info.html", {
+            "advert": advert,
+        })
