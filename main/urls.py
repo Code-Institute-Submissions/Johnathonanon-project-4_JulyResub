d69bffb0fc070c112d24b229cfdbdcd4d1f0a5de
Johnathonanon <jconry21@gmail.com>
2022-04-05 13:35:44 +0000

    Create advert info path and add bg image

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.AdvertList.as_view(), name='home')
+    path('', views.AdvertList.as_view(), name='home'),
+    path('<slug:slug>/', views.AdvertInfo.as_view(), name='advert_info')
 ]
