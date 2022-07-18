458a867eb72147f0998a6efc03d96aaf055b97d6
Johnathonanon <jconry21@gmail.com>
2022-07-18 16:15:26 +0000

    resubmission - fix ad cards and ad info

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('', views.AdvertList.as_view(), name='home'),
     path('my_adverts/', views.MyAdvertList.as_view(), name='my_adverts'),
     path('post_advert/', views.PostAdvert.as_view(), name='post_advert'),
-    path('<slug>_edit_advert/', views.UpdateAdvert.as_view(), name='edit_advert'),
-    path('<slug>/delete_advert', views.DeleteAdvert.as_view(), name='delete_advert'),
+    path('<slug>_edit_advert/',
+         views.UpdateAdvert.as_view(), name='edit_advert'),
+    path('<slug>/delete_advert',
+         views.DeleteAdvert.as_view(), name='delete_advert'),
     path('<slug:slug>/', views.AdvertInfo.as_view(), name='advert_info'),
 ]
